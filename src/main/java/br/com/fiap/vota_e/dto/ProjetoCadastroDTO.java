63d97d2f95504fb98495de71adc490ea3d6be947
Pedro <pedroarthuralvesdeoliveira@hotmail.com>
2024-10-20 16:21:16 -0300

    Implementação das validações personalizadas na camada DTO

--- src/main/java/br/com/fiap/vota_e/dto/ProjetoCadastroDTO.java
+++ src/main/java/br/com/fiap/vota_e/dto/ProjetoCadastroDTO.java
@@ -1,15 +1,21 @@
 package br.com.fiap.vota_e.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.util.Date;
 
 public record ProjetoCadastroDTO(
         Long projeto_id,
+        @NotBlank(message = "A descrição é obrigatória!")
         String descricao,
+        @NotBlank(message = "O título é obrigatório!")
         String titulo,
         String status,
         Date dataCadastro,
         Date dataEnvio,
         Date dataAprovacao,
+        @NotNull(message = "A sugestão é obrigatória!")
         Long sugestao_id
 ) {
 }
--- src/main/java/br/com/fiap/vota_e/dto/UsuarioCadastroDTO.java
+++ src/main/java/br/com/fiap/vota_e/dto/UsuarioCadastroDTO.java
@@ -1,8 +1,17 @@
 package br.com.fiap.vota_e.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record UsuarioCadastroDTO (
         Long usuarioId,
+        @NotBlank(message = "O nome de usuário é obrigatório!")
         String nome,
+        @NotBlank(message = "O e-mail do usuário é obrigatório!")
+        @Email(message = "O e-mail do usuário não é válido!")
         String email,
+        @NotBlank(message = "A senha é obrigatória!")
+        @Size(min = 6, max = 20, message = "A senha deve conter entre 6 e 20 caracteres!")
         String senha
 ) {}
